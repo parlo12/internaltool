ddcd6a684cb1d29d79c0522c5bba0cb560b3e2f1
eliudnyamai <eliudmitau@gmail.com>
2025-02-14 18:08:55 +0300

    Testing deployment pipeline

--- resources/js/Pages/Admin/Index.jsx
+++ resources/js/Pages/Admin/Index.jsx
@@ -400,7 +400,7 @@ export default function Index({
                                             Email
                                         </th>
                                         <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Admin Status
+                                            Admin Statu
                                         </th>
                                         <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                             Organisation
