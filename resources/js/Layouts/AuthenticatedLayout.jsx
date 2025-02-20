e6bdf45fe6ec359cf1cd468d49cc4ec0738b07da
eliudnyamai <eliudmitau@gmail.com>
2025-02-20 21:08:27 +0300

    wrong numbers table

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -21,11 +21,7 @@ export default function Authenticated({ user, header, children }) {
                             </div>
 
                             
-                            {/* <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('reports.index')} active={route().current('reports.index')}>
-                                    Call Reports
-                                </NavLink>
-                            </div> */}
+                            
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink href={route('create-workflow')} active={route().current('create-workflow')}>
                                     Workflow
