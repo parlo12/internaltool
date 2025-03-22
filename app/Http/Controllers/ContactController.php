9602a809a9dab07651c0e4a6c7d363a83c43e605
eliudnyamai <eliudmitau@gmail.com>
2025-03-23 00:18:23 +0300

    phone pool feature

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -160,70 +160,70 @@ class ContactController extends Controller
     { //Keep the commented code for the purpose of local testing. 
         //Uncomment when testing workflows offline
         //production uses a copy in routes/console.php so comment when pushing to prod
-        ini_set('max_execution_time', 0);
-        ini_set('memory_limit', '256M');
-        $steps = Step::where('created_at', '>=', now()->subMonth())->get();
-        foreach ($steps as $step) {
-            $workflow = Workflow::find($step->workflow_id);
-            $days_of_week = json_decode($step->days_of_week, true);
+        // ini_set('max_execution_time', 0);
+        // ini_set('memory_limit', '256M');
+        // $steps = Step::where('created_at', '>=', now()->subMonth())->get();
+        // foreach ($steps as $step) {
+        //     $workflow = Workflow::find($step->workflow_id);
+        //     $days_of_week = json_decode($step->days_of_week, true);
 
-            if ($workflow != null && $workflow->active) {
-                $contacts = DB::table('contacts')
-                    ->where('response', 'No')
-                    ->where('can_send', 1)
-                    ->where('subscribed', 1)
-                    ->where('current_step', $step->id)
-                    ->get();
-                $start_time = $step->start_time ?: '08:00';
-                $end_time = $step->end_time ?: '20:00';
-                $chunk_size = $step->batch_size ?: '20';
-                $interval = (int) $step->batch_delay * 60;
-                $contactsChunks = $contacts->chunk($chunk_size);
-                $now = Carbon::now();
-                $startTime = Carbon::today()->setTimeFromTimeString($start_time);
-                $endTime = Carbon::today()->setTimeFromTimeString($end_time);
-                if ($now->between($startTime, $endTime)) {
-                    $startTime = $now;
-                } elseif ($now->isAfter($endTime)) {
-                    $startTime = Carbon::tomorrow()->setTimeFromTimeString($start_time);
-                    $endTime = Carbon::tomorrow()->setTimeFromTimeString($end_time);
-                }
-                while (($days_of_week[$startTime->format('l')] ?? 0) == 0) {
-                    $startTime = $startTime->addDay()->setTimeFromTimeString($start_time);
-                    $endTime = $endTime->addDay();
-                }
-                foreach ($contactsChunks as $chunk) {
-                    if ($startTime->greaterThanOrEqualTo($endTime)) {
-                        do {
-                            $startTime = $startTime->addDay()->setTimeFromTimeString($start_time);
-                            $endTime = $endTime->addDay();
-                        } while (($days_of_week[$startTime->format('l')] ?? 0) == 0);
-                    }
+        //     if ($workflow != null && $workflow->active) {
+        //         $contacts = DB::table('contacts')
+        //             ->where('response', 'No')
+        //             ->where('can_send', 1)
+        //             ->where('subscribed', 1)
+        //             ->where('current_step', $step->id)
+        //             ->get();
+        //         $start_time = $step->start_time ?: '08:00';
+        //         $end_time = $step->end_time ?: '20:00';
+        //         $chunk_size = $step->batch_size ?: '20';
+        //         $interval = (int) $step->batch_delay * 60;
+        //         $contactsChunks = $contacts->chunk($chunk_size);
+        //         $now = Carbon::now();
+        //         $startTime = Carbon::today()->setTimeFromTimeString($start_time);
+        //         $endTime = Carbon::today()->setTimeFromTimeString($end_time);
+        //         if ($now->between($startTime, $endTime)) {
+        //             $startTime = $now;
+        //         } elseif ($now->isAfter($endTime)) {
+        //             $startTime = Carbon::tomorrow()->setTimeFromTimeString($start_time);
+        //             $endTime = Carbon::tomorrow()->setTimeFromTimeString($end_time);
+        //         }
+        //         while (($days_of_week[$startTime->format('l')] ?? 0) == 0) {
+        //             $startTime = $startTime->addDay()->setTimeFromTimeString($start_time);
+        //             $endTime = $endTime->addDay();
+        //         }
+        //         foreach ($contactsChunks as $chunk) {
+        //             if ($startTime->greaterThanOrEqualTo($endTime)) {
+        //                 do {
+        //                     $startTime = $startTime->addDay()->setTimeFromTimeString($start_time);
+        //                     $endTime = $endTime->addDay();
+        //                 } while (($days_of_week[$startTime->format('l')] ?? 0) == 0);
+        //             }
 
-                    $dispatchTime = $startTime->copy();
-                    foreach ($chunk as $contact) {
-                        // Dispatch a job to prepare the message without making third-party requests here
-                        PrepareMessageJob::dispatch(
-                            $contact->uuid,
-                            $workflow->group_id,
-                            $workflow->godspeedoffers_api,
-                            $step,
-                            $contact,
-                            $dispatchTime
-                        );
-                        $contact = Contact::find($contact->id);
-                        $contact->can_send = 0;
-                        $contact->status = 'Waiting_For_Queau_Job';
-                        $contact->save();
-                    }
-                    $startTime->addSeconds($interval);
-                    while (($days_of_week[$startTime->format('l')] ?? 0) == 0) {
-                        $startTime = $startTime->addDay()->setTimeFromTimeString($start_time);
-                        $endTime = $endTime->addDay();
-                    }
-                }
-            }
-        }
+        //             $dispatchTime = $startTime->copy();
+        //             foreach ($chunk as $contact) {
+        //                 // Dispatch a job to prepare the message without making third-party requests here
+        //                 PrepareMessageJob::dispatch(
+        //                     $contact->uuid,
+        //                     $workflow->group_id,
+        //                     $workflow->godspeedoffers_api,
+        //                     $step,
+        //                     $contact,
+        //                     $dispatchTime
+        //                 );
+        //                 $contact = Contact::find($contact->id);
+        //                 $contact->can_send = 0;
+        //                 $contact->status = 'Waiting_For_Queau_Job';
+        //                 $contact->save();
+        //             }
+        //             $startTime->addSeconds($interval);
+        //             while (($days_of_week[$startTime->format('l')] ?? 0) == 0) {
+        //                 $startTime = $startTime->addDay()->setTimeFromTimeString($start_time);
+        //                 $endTime = $endTime->addDay();
+        //             }
+        //         }
+        //     }
+        // }
     }
 
     public function send_message($phone, $content, $workflow_id, $type, $contact_id, $organisation_id)
