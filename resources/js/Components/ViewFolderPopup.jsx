f80f02a600fe279a0a4f735d68a58dde6dce3cc2
eliudnyamai <eliudmitau@gmail.com>
2025-04-24 09:43:10 +0300

    AI calls to prod

--- resources/js/Components/ViewFolderPopup.jsx
+++ resources/js/Components/ViewFolderPopup.jsx
@@ -51,75 +51,77 @@ const CopyWorkflowPopup = ({
                         <div className="max-w-full sm:px-6 lg:px-8">
                             <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                                 <div className="p-4 sm:p-6 bg-white border-b border-gray-200 overflow-x-auto">
-                                    <table className="min-w-full divide-y divide-gray-200 text-sm sm:text-base">
-                                        <thead>
-                                            <tr>
-                                                <th className="px-4 sm:px-6 py-3 bg-gray-50 text-left font-medium text-gray-700 uppercase tracking-wider">
-                                                    Name
-                                                </th>
-                                                <th className="px-4 sm:px-6 py-3 bg-gray-50 text-left font-medium text-gray-700 uppercase tracking-wider">
-                                                    Contact Group
-                                                </th>
-                                                <th className="px-4 sm:px-6 py-3 bg-gray-50 text-left font-medium text-gray-700 uppercase tracking-wider">
-                                                    Actions
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody className="bg-white divide-y divide-gray-200">
-                                            {workflowData.map((workflow) => (
-                                                <tr key={workflow.id}>
-                                                    <td className="px-4 sm:px-6 py-4 whitespace-nowrap text-gray-900">
-                                                        {workflow.name}
-                                                    </td>
-                                                    <td className="px-4 sm:px-6 py-4 whitespace-nowrap text-gray-500">
-                                                        {workflow.contact_group}
-                                                    </td>
-                                                    <td className="px-4 sm:px-6 py-4 whitespace-nowrap text-right">
-                                                        <button
-                                                            onClick={() =>
-                                                                handleCopyClick(
-                                                                    workflow
-                                                                )
-                                                            }
-                                                            className="inline-flex items-center px-3 sm:px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mr-2"
-                                                        >
-                                                            <FontAwesomeIcon
-                                                                icon={faCopy}
-                                                                className="fa-xs"
-                                                            />
-                                                        </button>
-                                                        <button
-                                                            onClick={() =>
-                                                                handleAssignFolder(
-                                                                    workflow
-                                                                )
-                                                            }
-                                                            className="inline-flex items-center px-3 sm:px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mr-2"
-                                                        >
-                                                            <FontAwesomeIcon
-                                                                icon={
-                                                                    faFolderOpen
-                                                                }
-                                                                className="fa-xs"
-                                                            />
-                                                        </button>
-                                                        <Link
-                                                            href={route(
-                                                                "add_steps",
-                                                                workflow.id
-                                                            )}
-                                                            className="inline-flex items-center px-3 sm:px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                                                        >
-                                                            <FontAwesomeIcon
-                                                                icon={faPen}
-                                                                className="fa-xs"
-                                                            />
-                                                        </Link>
-                                                    </td>
+                                    <div className="max-h-96 overflow-y-auto">
+                                        <table className="min-w-full divide-y divide-gray-200 text-sm sm:text-base">
+                                            <thead>
+                                                <tr>
+                                                    <th className="px-4 sm:px-6 py-3 bg-gray-50 text-left font-medium text-gray-700 uppercase tracking-wider">
+                                                        Name
+                                                    </th>
+                                                    <th className="px-4 sm:px-6 py-3 bg-gray-50 text-left font-medium text-gray-700 uppercase tracking-wider">
+                                                        Contact Group
+                                                    </th>
+                                                    <th className="px-4 sm:px-6 py-3 bg-gray-50 text-left font-medium text-gray-700 uppercase tracking-wider">
+                                                        Actions
+                                                    </th>
                                                 </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
+                                            </thead>
+                                            <tbody className="bg-white divide-y divide-gray-200">
+                                                {workflowData.map((workflow) => (
+                                                    <tr key={workflow.id}>
+                                                        <td className="px-4 sm:px-6 py-4 whitespace-nowrap text-gray-900">
+                                                            {workflow.name}
+                                                        </td>
+                                                        <td className="px-4 sm:px-6 py-4 whitespace-nowrap text-gray-500">
+                                                            {workflow.contact_group}
+                                                        </td>
+                                                        <td className="px-4 sm:px-6 py-4 whitespace-nowrap text-right">
+                                                            <button
+                                                                onClick={() =>
+                                                                    handleCopyClick(
+                                                                        workflow
+                                                                    )
+                                                                }
+                                                                className="inline-flex items-center px-3 sm:px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mr-2"
+                                                            >
+                                                                <FontAwesomeIcon
+                                                                    icon={faCopy}
+                                                                    className="fa-xs"
+                                                                />
+                                                            </button>
+                                                            <button
+                                                                onClick={() =>
+                                                                    handleAssignFolder(
+                                                                        workflow
+                                                                    )
+                                                                }
+                                                                className="inline-flex items-center px-3 sm:px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mr-2"
+                                                            >
+                                                                <FontAwesomeIcon
+                                                                    icon={
+                                                                        faFolderOpen
+                                                                    }
+                                                                    className="fa-xs"
+                                                                />
+                                                            </button>
+                                                            <Link
+                                                                href={route(
+                                                                    "add_steps",
+                                                                    workflow.id
+                                                                )}
+                                                                className="inline-flex items-center px-3 sm:px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                                                            >
+                                                                <FontAwesomeIcon
+                                                                    icon={faPen}
+                                                                    className="fa-xs"
+                                                                />
+                                                            </Link>
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         </div>
