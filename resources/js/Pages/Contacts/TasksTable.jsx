6607b75d0b8d4380b9bb619c1d91374fc4abc682
eliudnyamai <eliudmitau@gmail.com>
2025-04-26 11:36:19 +0300

    ui fix

--- resources/js/Pages/Contacts/TasksTable.jsx
+++ resources/js/Pages/Contacts/TasksTable.jsx
@@ -56,14 +56,14 @@ export default function TasksTable({
     return (
         <>
             {success && (
-                <div className="bg-emerald-500 py-2 px-4 text-white rounded mb-4 text-sm font-medium">
+                <div className="bg-emerald-500 py-2 px-4 text-white rounded mb-4 shadow-md">
                     {success}
                 </div>
             )}
             <div className="overflow-x-auto shadow-md rounded-lg">
-                <table className="w-full text-sm text-left text-gray-700">
-                    <thead className="text-xs text-gray-600 uppercase bg-gray-100">
-                        <tr>
+                <table className="w-full text-sm text-left rtl:text-right border-collapse border border-gray-200">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-100 border-b border-gray-300">
+                        <tr className="text-nowrap">
                             <TableHeading
                                 name="id"
                                 sort_field={queryParams.sort_field}
@@ -120,16 +120,28 @@ export default function TasksTable({
                             >
                                 Next Step on
                             </TableHeading>
+
+                            <TableHeading
+                                name="created_at"
+                                sort_field={queryParams.sort_field}
+                                sort_direction={queryParams.sort_direction}
+                                sortChanged={sortChanged}
+                            >
+                                Create Date
+                            </TableHeading>
+                            <th>Actions</th>
                         </tr>
                     </thead>
-                    <thead className="bg-gray-50">
-                        <tr>
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-100">
+                        <tr className="text-nowrap">
                             <th className="px-3 py-3"></th>
                             <th className="px-3 py-3">
                                 <TextInput
-                                    className="w-full border-gray-300 rounded-md"
-                                    defaultValue={queryParams.contact_name || ""}
-                                    placeholder="Contact Name"
+                                    className="w-full"
+                                    defaultValue={
+                                        queryParams.contact_name || ""
+                                    }
+                                    placeholder="contact name"
                                     onBlur={(e) =>
                                         searchFieldChanged(
                                             "contact_name",
@@ -143,7 +155,7 @@ export default function TasksTable({
                             </th>
                             <th className="px-3 py-3">
                                 <TextInput
-                                    className="w-full border-gray-300 rounded-md"
+                                    className="w-full"
                                     defaultValue={queryParams.phone || ""}
                                     placeholder="Phone"
                                     onBlur={(e) =>
@@ -157,7 +169,7 @@ export default function TasksTable({
                             </th>
                             <th className="px-3 py-3">
                                 <SelectInput
-                                    className="w-full border-gray-300 rounded-md"
+                                    className="w-full"
                                     defaultValue={queryParams.response || ""}
                                     onChange={(e) =>
                                         searchFieldChanged(
@@ -171,12 +183,13 @@ export default function TasksTable({
                                     <option value="no">NO</option>
                                 </SelectInput>
                             </th>
+
                             {!hideProjectColumn && (
                                 <th className="px-3 py-3"></th>
                             )}
                             <th className="px-3 py-3">
                                 <SelectInput
-                                    className="w-full border-gray-300 rounded-md"
+                                    className="w-full"
                                     defaultValue={
                                         queryParams.queaue_status || ""
                                     }
@@ -196,12 +209,13 @@ export default function TasksTable({
                                 </SelectInput>
                             </th>
                             <th className="px-3 py-3"></th>
+                            <th className="px-3 py-3"></th>
                         </tr>
                     </thead>
                     <tbody>
                         {contacts.data.map((contact) => (
                             <tr
-                                className="bg-white text-gray-700 border-b hover:bg-gray-50"
+                                className="bg-white text-gray-800 border-b hover:bg-gray-50 transition duration-150"
                                 key={contact.id}
                             >
                                 <td className="px-3 py-2">{contact.id}</td>
@@ -210,15 +224,90 @@ export default function TasksTable({
                                         {contact.contact_name}
                                     </td>
                                 )}
+
                                 <td className="px-3 py-2">
-                                    <span className="px-2 py-1 rounded bg-gray-100">
+                                    <span className="px-2 py-1 rounded text-nowrap bg-gray-100">
                                         {contact.phone}
                                     </span>
                                 </td>
-                                <td className="px-3 py-2">{contact.response}</td>
-                                <td className="px-3 py-2">{contact.current_step}</td>
-                                <td className="px-3 py-2">{contact.status}</td>
-                                <td className="px-3 py-2">{contact.can_send_after}</td>
+                                <td className="px-3 py-2 text-nowrap">
+                                    {contact.response}
+                                </td>
+                                <td className="px-3 py-2 text-nowrap">
+                                    {contact.current_step}
+                                </td>
+                                <td className="px-3 py-2 text-nowrap">
+                                    {contact.status}
+                                </td>
+                                <td className="px-3 py-2 text-nowrap">
+                                    {contact.can_send_after}
+                                </td>
+                                <td className="px-3 py-2">
+                                    {contact.created_at}
+                                </td>
+                                <td className="">
+                                    <div className="flex mb-1">
+                                        <Link
+                                            href={route(
+                                                "mark-lead",
+                                                contact.id
+                                            )}
+                                            className={`bg-blue-600 hover:bg-blue-300 text-white text-nowrap mr-1 ${contact.valid_lead == 1
+                                                ? "bg-red-600 hover:bg-red-300"
+                                                : ""
+                                                }`}
+                                        >
+                                            {contact.valid_lead == 1
+                                                ? "Unmark as lead"
+                                                : "Mark Lead"}
+                                        </Link>{" "}
+                                        <Link
+                                            href={route(
+                                                "mark-offer",
+                                                contact.id
+                                            )}
+                                            className={`bg-blue-600 hover:bg-blue-300 text-white text-nowrap mr-1 ${contact.offer_made == 1
+                                                ? "bg-red-600 hover:bg-red-300"
+                                                : ""
+                                                }`}
+                                        >
+                                            {contact.offer_made == 1
+                                                ? "Unmark as offer"
+                                                : "Mark offer"}
+                                        </Link>
+                                        <Link
+                                            href={route(
+                                                "execute-contract",
+                                                contact.id
+                                            )}
+                                            className={`bg-blue-600 hover:bg-blue-300 text-white text-nowrap mr-1 ${contact.contract_executed == 1
+                                                ? "bg-red-600 hover:bg-red-300"
+                                                : ""
+                                                }`}
+                                        >
+                                            {contact.contract_executed == 1
+                                                ? "Unxecute contract"
+                                                : "execute contract"}
+                                        </Link>
+                                    </div>
+                                    <div className="flex">
+                                        <Link
+                                            href={route(
+                                                "close-deal",
+                                                contact.id
+                                            )}
+                                            className={`bg-blue-600 hover:bg-blue-300 text-white text-nowrap mr-1 ${contact.deal_closed == 1
+                                                ? "bg-red-600 hover:bg-red-300"
+                                                : ""
+                                                }`}
+                                        >
+                                            {contact.deal_closed == 1
+                                                ? "Unclose Deal"
+                                                : "Close Deal"}{" "}
+
+                                        </Link>
+                                    </div>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
@@ -226,13 +315,18 @@ export default function TasksTable({
                 <div className="flex justify-center my-4">
                     <a
                         href={exportUrl}
-                        className="px-4 py-2 text-white bg-green-600 hover:bg-green-500 rounded-md text-sm"
+                        className="text-center mt-2 w-1/4 text-white bg-green-600 hover:bg-green-500 px-4 py-2 rounded-lg shadow-md transition duration-150"
                     >
                         Export Contacts with No Response
                     </a>
                 </div>
             </div>
-            <Pagination links={contacts.meta.links} queryParams={queryParams} />
+
+            <Pagination
+                links={contacts.meta.links}
+                queryParams={queryParams}
+                className="mt-4"
+            />
         </>
     );
 }
