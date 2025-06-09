89ed20b24b5fc754238ec69267d9969062ddb1a7
eliudnyamai <eliudmitau@gmail.com>
2025-06-09 22:54:58 +0300

    Progress reporting done

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,9 +6,9 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    wsHost: import.meta.env.VITE_REVERB_HOST, // e.g. workflowtoolstaging.godspeedoffers.com
+    wsPath: '/app', // this is CRUCIAL so it knows to use `/app` instead of root
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
+
