01ea31e497c2abcbb5be53bcd573594208dc16ed
eliudnyamai <eliudmitau@gmail.com>
2025-08-08 21:36:27 +0300

    Working on email attachments

--- app/Http/Controllers/WorkflowController.php
+++ app/Http/Controllers/WorkflowController.php
@@ -377,7 +377,8 @@ class WorkflowController extends Controller
                             'step_quota_balance' => $step_to_copy->step_quota_balance,
                             'days_of_week' => $step_to_copy->days_of_week,
                             'generated_message' => $step_to_copy->generated_message,
-                            'email_subject' => $step_to_copy->email_subject
+                            'email_subject' => $step_to_copy->email_subject,
+                            'selected_field_ids' => $step_to_copy->selected_file_ids,
 
                         ]);
                         $new_steps_flow = $new_workflow->steps_flow ? explode(',', $new_workflow->steps_flow) : [];
