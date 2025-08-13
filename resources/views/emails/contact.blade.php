3171b8308f0eadb8cb539bfb3dba02318f9be32b
eliudnyamai <eliudmitau@gmail.com>
2025-08-13 22:55:18 +0300

    Working on email attachments

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,27 +15,35 @@
             padding: 20px;
             background-color: #ffffff;
         }
+
         p {
             margin: 0 0 16px 0;
         }
+
         .email-container {
             max-width: 600px;
             margin: 0 auto;
+            text-align: left;
+            /* ensures left alignment */
         }
+
         @media only screen and (max-width: 620px) {
             .email-container {
                 width: 100%;
             }
         }
     </style>
+
 </head>
+
 <body>
     <div class="email-container">
         {!! nl2br(e($details['message'])) !!}
-        
-        @if(isset($details['signature']))
+
+        @if (isset($details['signature']))
             <p>{!! nl2br(e($details['signature'])) !!}</p>
         @endif
     </div>
 </body>
+
 </html>
