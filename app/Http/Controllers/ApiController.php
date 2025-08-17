3ba222d9adfef1cbe239234c2864c62a18489be5
eliudnyamai <eliudmitau@gmail.com>
2025-08-17 22:55:27 +0300

    Working on email attachments

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -559,8 +559,7 @@ class ApiController extends Controller
             Log::info("Contact not found for phone: $phone in organisation: $user->organisation_id");
             return response()->json(['error' => 'Contact not found'], 404);
         }
-        Log::info("Contact found: ", (array)$contact);
-        Log::info("contact found in organisation: $user->organisation_id");
+       
         $current_step = $contact->current_step;
         $step = Step::find($current_step);
     
