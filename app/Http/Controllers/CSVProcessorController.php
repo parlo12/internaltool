d7fd9e76c4daf8e7f07fbe79c16c6d3320c774b9
eliudnyamai <eliudmitau@gmail.com>
2025-09-13 03:23:58 +0300

    Working on contacts import

--- app/Http/Controllers/CSVProcessorController.php
+++ app/Http/Controllers/CSVProcessorController.php
@@ -51,14 +51,15 @@ class CSVProcessorController extends Controller
     }
     public function import(Request $request)
     {
-
-        try {
-            $validated = $request->validate([
+        $validated = $request->validate([
                 'mappings' => 'required|array',
                 'data' => 'required|array',
                 'selected_workflow_id' => 'required|exists:workflows,id',
                 'filename' => 'required|string',
             ]);
+
+        try {
+            
             $contact_import_progress = ContactImportProgress::create([
                 'user_id' => auth()->user()->id,
                 'processed_contacts' => 0,
