cb84e0b5499f5b97eb44dda6369bc77e90764dfa
eliudnyamai <eliudmitau@gmail.com>
2025-09-15 23:29:39 +0300

    Working on contacts import

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -36,7 +36,7 @@ export default function Authenticated({ user, header, children }) {
                                         <span className="inline-flex rounded-md  space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                             <button
                                                 type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                                className="inline-flex items-center px-3 py-6 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
                                             >
                                                 Upload CSV
                                                 <svg
